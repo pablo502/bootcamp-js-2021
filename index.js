0a7e63c59c54a5d17baafd7350e6187d05fb0b5c
pablo502_ <pabloalvarez.cb@hotmail.com>
2021-11-28 22:33:28 -0600

    Primer Commit

--- index.js
+++ index.js
@@ -1,12 +1,114 @@
-const paragraphs = document.getElementsByTagName("p");
+const form = document.getElementsByTagName("form")[0];
+const tbody = document.getElementsByTagName("tbody")[0];
+/** @type {HTMLInputElement} */
+const txtNombre = document.getElementById("nombre");
+const cantidadTotalElement = document.getElementById("cantidad-total");
+const precioTotalElement = document.getElementById("precio-total");
+const granTotalElement = document.getElementById("gran-total");
+/** @type {HTMLInputElement} */
+const txtPrecio = document.getElementById("precio");
+/** @type {HTMLInputElement} */
+const txtCantidad = document.getElementById("cantidad");
+/** @type {HTMLInputElement} */
+const selectedCategoria = document.getElementById("categoria");
+/** @type {HTMLInputElement} */
+const txtCodigo = document.getElementById("codigo");
 
-if (paragraphs.length > 0) {
-    const paragraph = paragraphs[0];
-    paragraph.innerHTML = "Bienvenidos al Bootcamp!";
+let indice = 0;
+let cantidadTotal = 0;
+let preciosTotales = 0;
+let granTotal = 0;
+let currentRow;
+
+form.addEventListener("submit", onSubmit);
+
+/**
+ * 
+ * @param {Event} event 
+ */
+function onSubmit(event) {
+
+   event.preventDefault();
+
+   const data = new FormData(form);
+   const values = Array.from(data.entries());
+
+   const [frmCodigo, frmNombre, frmCantidad, frmPrecio, frmCategoria] = values;
+
+   let codigo = frmCodigo[1];
+   const nombre = frmNombre[1];
+   const cantidad = frmCantidad[1];
+   const precio = frmPrecio[1];
+   const categoria = frmCategoria[1];
+   const total = cantidad * precio;
+
+   cantidadTotal = parseFloat(cantidad) + cantidadTotal;
+   preciosTotales = parseFloat(precio) + preciosTotales;
+   granTotal = parseFloat(total) + granTotal;
+
+   let tr;
+
+   if (!codigo) {
+       indice++;
+       codigo = indice;
+       tr = document.createElement("tr");
+       tbody.appendChild(tr);
+   } else {
+       tr = currentRow;
+   }
+   
+   tr.dataset.categoria = categoria;
+   tr.innerHTML = `
+        <td>${codigo}</td>
+        <td>${nombre}</td>
+        <td>${cantidad}</td>
+        <td>${precio}</td>
+        <td>${total}</td>
+        <td><a href="#" onclick="onEdit(event)">Editar</a> | <a href="#" onclick="onDelete(event)">Eliminar</a></td></td>
+    `;
+
+   
+
+   cantidadTotalElement.innerText = cantidadTotal;
+   precioTotalElement.innerText = preciosTotales;
+   granTotalElement.innerText = granTotal;
+
+   form.reset();
+   txtNombre.focus();
 }
 
-if (paragraphs.length > 1) {
-    const paragraph = paragraphs[1];
-    const fecha = new Date();
-    paragraph.innerHTML = "Parrafos en el documento: " + paragraphs.length + " (" + fecha + ")";
+/**
+ * 
+ * @param {Event} event 
+ */
+function onEdit(event) {
+
+    event.preventDefault();
+    /** @type {HTMLElement} */
+    const anchor = event.target;
+    const tr = anchor.parentElement.parentElement;
+    const celdas = tr.getElementsByTagName("td");
+    const [tdCodigo, tdNombre, tdCantidad, tdPrecio] = celdas;
+
+    txtCodigo.value = tdCodigo.innerText;
+    txtNombre.value = tdNombre.innerText;
+    txtCantidad.value = tdCantidad.innerText;
+    txtPrecio.value = tdPrecio.innerText;
+    selectedCategoria.value = tr.dataset.categoria;
+
+    currentRow = tr;
+    console.log(celdas);
+}
+
+/**
+ * 
+ * @param {Event} event 
+ */
+function onDelete(event) {
+
+    event.preventDefault();
+    /** @type {HTMLElement} */
+    const anchor = event.target;
+    const tr = anchor.parentElement.parentElement;
+    tbody.removeChild(tr);
 }
